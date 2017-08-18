f40c20658924d1028a53a51bfce8650dc0b8d4ce
Yusuke Shinyama <yusuke@shinyama.jp>
2017-08-18 20:03:19 +0900

    setup.py tweaks.

--- setup.py
+++ setup.py
@@ -3,17 +3,22 @@ import sys
 from cx_Freeze import setup, Executable
 
 build_exe_options = {
+    'optimize': 2,
     'packages': ['idna'],
     'includes': ['_cffi_backend'],
     'include_files': ['icons'],
 }
 
-base = None
-if sys.platform == 'win32':
-    base = 'Win32GUI'
-
-setup(name = 'PyRexecd',
-      version = '0.1',
-      description = 'PyRexecd',
-      options = { 'build_exe': build_exe_options },
-      executables = [Executable('PyRexecd.py', base=base, icon='icons/PyRexec.ico')])
+exe = Executable(
+    'PyRexecd.py',
+    base='Win32GUI',
+    icon='icons/PyRexec.ico'
+)
+    
+setup(
+    name = 'PyRexecd',
+    version = '0.1',
+    description = 'PyRexecd',
+    options = { 'build_exe': build_exe_options },
+    executables = [exe]
+)
