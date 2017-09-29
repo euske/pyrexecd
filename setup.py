fe77ff1b4bac62bca8df8b9a2926ca2d409451d1
Yusuke Shinyama <yusuke@shinyama.jp>
2017-09-29 21:13:05 +0900

    Added pip support.

--- setup.py
+++ setup.py
@@ -11,14 +11,30 @@ build_exe_options = {
 
 exe = Executable(
     'PyRexecd.py',
-    base='Win32GUI',
-    icon='icons/PyRexec.ico'
+    base = 'Win32GUI',
+    icon = 'icons/PyRexec.ico'
 )
     
 setup(
     name = 'PyRexecd',
-    version = '0.1',
-    description = 'PyRexecd',
+    version = '0.2',
+    description = 'Standalone SSH server for Windows',
+    url = 'https://github.com/euske/pyrexecd',
+    author = 'Yusuke Shinyama',
+    author_email = 'yusuke@shinyama.jp',
+    license = 'MIT',
+    packages = [],
+    install_requires = [
+        'paramiko',
+        'pypiwin32',
+        'cx_Freeze',
+    ],
+    executables = [exe],
     options = { 'build_exe': build_exe_options },
-    executables = [exe]
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Win32 (MS Windows)',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Utilities',
+    ],
 )
